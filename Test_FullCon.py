444b3b8afca4d34047b218bac714050aef118791
lemoon <wumaomamolemoon@gmail.com>
2022-01-26 01:22:32 +0800

    [improve] improve the code to save weight datas

--- Test_FullCon.py
+++ Test_FullCon.py
@@ -6,78 +6,25 @@ from torchvision import datasets, transforms
 from torch.autograd import Variable
 from torch.utils import data
 from torch.utils.tensorboard import SummaryWriter
-
-# super parameters
-picture_feature = 16*16
-picture_hidden1 = 32
-picture_hidden2 = 32
-picture_output = 10
-learning_rate = 0.01
-BATCH_SIZE = 1
-EPOCHS = 100
-
-
-# data loading
-train_transformer = transforms.Compose([
-    # transforms.ToPILImage(),
-    transforms.Resize(16),
-    # transforms.RandomHorizontalFlip(),
-    transforms.ToTensor()
-    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
-
-train_loader = data.DataLoader(
-    datasets.MNIST('data', train=True, download=True, transform=train_transformer),
-    batch_size=BATCH_SIZE, shuffle=True)
-
-test_loader = data.DataLoader(
-    datasets.MNIST('data', train=False, download=True, transform=train_transformer),
-    batch_size=BATCH_SIZE, shuffle=True)
-
-
-# Network structure
-class Net(nn.Module):
-
-    def __init__(self, n_feature, n_hidden1, n_hidden2, n_output):
-        super(Net, self).__init__()
-        self.hidden1 = nn.Linear(n_feature, n_hidden1, bias=False)
-        self.hidden2 = nn.Linear(n_hidden1, n_hidden2, bias=False)
-        self.out = nn.Linear(n_hidden2, n_output)
-
-    def forward(self, x):
-        x = torch.tanh(self.hidden1(x))
-        x = torch.tanh(self.hidden2(x))
-        x = torch.tanh(self.out(x))
-        return x
-
-
-# create a network sample
-net = Net(n_feature=picture_feature, n_hidden1=picture_hidden1, n_hidden2=picture_hidden2, n_output=picture_output)
-
-# loss function and optimizer
-Loss_function = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=learning_rate)
-
-# tensor vision
-writer = SummaryWriter('D:\\Python_projects\\StochasticNet\\FullConnect_Mnist')
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 
 # train function
-def train(module, train_data, optimizer_func, epoch_num):
+def train(module, train_data, optimizer_function, epoch_num):
     for batch_idx, (t_data, target) in enumerate(train_data):
         t_data = t_data.view(t_data.size(0), -1)
-        # t_data = t_data.view(picture_feature, -1)
-        t_data, target = Variable(t_data), Variable(target)
-        optimizer_func.zero_grad()
+        t_data, target = Variable(t_data).to(device), Variable(target).to(device)
+        optimizer_function.zero_grad()
         output = module(t_data)
         loss = Loss_function(output, target)
         loss.backward()
-        optimizer_func.step()
+        optimizer_function.step()
         if batch_idx % 300 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch_num, batch_idx * len(t_data), len(train_data.dataset),
                 100. * batch_idx / len(train_data), loss.item()))
-            # writer.add_scalar('train_loss{}'.format(epoch_num), loss.item(), batch_idx)
 
 
 # test function
@@ -86,19 +33,97 @@ def test(model, test_data, epoch_num):
     with torch.no_grad():
         for t_data, target in test_data:
             t_data = t_data.view(t_data.size(0), -1)
-            t_data, target = Variable(t_data), Variable(target)
+            t_data, target = Variable(t_data).to(device), Variable(target).to(device)
             output = model(t_data)
             pred = output.max(1, keepdim=True)[1]  # 找到概率最大的下标
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     print("\nTest set: Epoch:{} Accuracy: {}/{} ({:.2f}%) \n".format(epoch_num, correct, len(test_data.dataset),
                                                                      100. * correct / len(test_data.dataset)))
-    writer.add_scalar('Accuracy/test', 100. * correct / len(test_data.dataset), epoch_num)
+    if 'writer' in locals().keys():
+        writer.add_scalar('Accuracy/test', 100. * correct / len(test_data.dataset), epoch_num)
+
+# Network structure
+class Net(nn.Module):
+
+    def __init__(self, n_feature, n_hidden1, n_hidden2, n_output):
+        super(Net, self).__init__()
+        self.hidden1 = nn.Linear(n_feature, n_hidden1, bias=False)
+        self.hidden2 = nn.Linear(n_hidden1, n_hidden2, bias=False)
+        self.out = nn.Linear(n_hidden2, n_output, bias=False)
+
+    # connect inputs and outputs
+    def forward(self, x):
+        x = torch.tanh(self.hidden1(x))
+        x = torch.tanh(self.hidden2(x))
+        x = torch.tanh(self.out(x))
+        return x
 
 
-t1 = time.time()
-for epoch in range(1, EPOCHS + 1):
-    train(module=net, train_data=train_loader, optimizer_func=optimizer, epoch_num=epoch)
-    test(model=net, test_data=test_loader, epoch_num=epoch)
-t2 = time.time()
-print('All time consumed :{:.1f}s'.format(t2 - t1))
+# hyper parameters
+size_inputs = 16*16
+size_hidden1 = 32
+size_hidden2 = 32
+size_outputs = 10
+learning_rate = 0.01
+BATCH_SIZE = 1
+EPOCHS = 1000
+
+
+if __name__ == '__main__':
+    # data precoding
+    train_transformer = transforms.Compose([
+        transforms.Resize(16), # down sampling
+        transforms.ToTensor()
+    ])
+
+    # data loading
+    train_loader = data.DataLoader(
+        datasets.MNIST('data', train=True, download=True, transform=train_transformer),
+        batch_size=BATCH_SIZE, shuffle=True)
+
+    test_loader = data.DataLoader(
+        datasets.MNIST('data', train=False, download=True, transform=train_transformer),
+        batch_size=BATCH_SIZE, shuffle=True)
+
+    # cuda acceleration
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = 'cpu'
+    print(f"Using {device} device")
+    
+    # create a network sample
+    net = Net(n_feature=size_inputs, n_hidden1=size_hidden1, n_hidden2=size_hidden2, n_output=size_outputs).to(device)
+
+    # loss function and optimizer
+    Loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+
+    # check for data
+    # for batch_idx, (t_data, target) in enumerate(train_loader):
+    #     t_data = t_data.view(16, 16)
+    #     plt.figure()
+    #     plt.imshow(t_data)
+    #     plt.show()
+
+    # tensorboard
+    writer = SummaryWriter('D:\\Python_projects\\StochasticNet\\FullConnect_Mnist')
+    for epoch in range(1, EPOCHS + 1):
+        train(module=net, train_data=train_loader, optimizer_function=optimizer, epoch_num=epoch)
+        test(model=net, test_data=test_loader, epoch_num=epoch)
+        print(net.state_dict().keys())
+        hidden1_weight = net.state_dict()['hidden1.weight'].numpy()
+        hidden2_weight = net.state_dict()['hidden2.weight'].numpy()
+        out_weight = net.state_dict()['out.weight'].numpy()
+        # print(f'hidden1_weight: {hidden1_weight}')
+        # print(f'hidden2_weight: {hidden2_weight}')
+        # print(f'out_weight: {out_weight}')
+        data_save_path = f'weight_data\\epoch_{epoch}'
+        if not os.path.exists(data_save_path):
+            os.makedirs(data_save_path)
+        # save data
+        np.save(f'{data_save_path}\\hidden1', hidden1_weight)
+        np.save(f'{data_save_path}\\hidden2', hidden2_weight)
+        np.save(f'{data_save_path}\\out_weight', out_weight)
+        print(f'hidden1 max: {np.max(hidden1_weight)} min: {np.min(hidden1_weight)}')
+        print(f'hidden2 max: {np.max(hidden2_weight)} min: {np.min(hidden2_weight)}')
+        print(f'out_weight max: {np.max(out_weight)} min: {np.min(out_weight)}')
